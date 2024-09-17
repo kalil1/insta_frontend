eef2301d3c98b21c854dd05879ae351a9d7099a9
kalil1 <wkalil77@gmail.com>
2024-09-16 23:35:09 -0400

    feat: settings form

---
+++ src/components/SettingsForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const UserSettingsForm = () => (
+  <div className="card">
+    <div className="card-body">
+      <div className="row">
+        <div className="col-md-12">
+          <h4>Settings</h4>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          <form>
+            {/* Settings form fields */}
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default UserSettingsForm;
