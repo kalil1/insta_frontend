e4aece27db4d58a6d92021970a9daf293204473f
kalil1 <wkalil77@gmail.com>
2025-06-10 22:49:41 -0400

    added useRef

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,6 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createConsumer } from "@rails/actioncable";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavLink } from 'react-router-dom';
 import axios from "axios";
 
